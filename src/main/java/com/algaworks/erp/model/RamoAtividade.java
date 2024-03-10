daab0e0ccf0b62302f7a8fb16407ca3875fb8417
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2024-03-10 15:48:29 -0300

    Inicializando projeto jsf e modelando entidades

---
+++ src/main/java/com/algaworks/erp/model/RamoAtividade.java
@@ -0,0 +1,55 @@
+package com.algaworks.erp.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.Id;
+
+
+@Entity
+@Table(name = "ramo_atividade")
+public class RamoAtividade implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	private String descricao;
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(Id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RamoAtividade other = (RamoAtividade) obj;
+		return Objects.equals(Id, other.Id);
+	}
+	@Override
+	public String toString() {
+		return "RamoAtividade [Id=" + Id + "]";
+	}
+	
+	
+	
+}
